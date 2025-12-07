0fcb90d83f301872f98f89f8d4c1d3e514f0647c
Abdul Moeed <abdulmoeedjhelumi@gmail.com>
2025-12-07 16:57:32 +0500

    Input component changes

--- src/components/ui/inputs/Input.tsx
+++ src/components/ui/inputs/Input.tsx
@@ -1,18 +1,34 @@
+import { useState } from "react"
+
 interface inputProps {
     value: string
     type?: string
     onChange: ChangeEventHandler<HTMLInputElement>
-    label: string
-    placeholder ? :string
-    className? : string
+    label?: string
+    innerLabel?: string
+    placeholder?: string
+    className?: string
 }
 
-export const Input = ({ value, onChange, type , label , placeholder , className}: inputProps) => {
+export const Input = ({ value, onChange, type, label, placeholder, className, innerLabel }: inputProps) => {
+    const [isFocused, setIsFocused] = useState<boolean>(false)
+    console.log("value" , value);
+    
     return (
         <>
             <div className="flex flex-col gap-3 w-full ">
-                <label htmlFor="">{label}</label>
-                <input onChange={onChange} type={type} value={value} placeholder={placeholder} className={`${className} w-full h-12 rounded-lg border border-border px-4`}></input>
+                {label && <label htmlFor="">{label}</label>}
+                <div className="w-full relative">
+
+                    <input onChange={onChange} type={type} value={value}
+                        placeholder={placeholder}
+                        onFocus={() => { setIsFocused(true) }}
+                        onBlur={() => { setIsFocused(false) }}
+                        className={`${className} w-full h-12 rounded-lg border border-border px-4 ${innerLabel && "h-14 pt-3"}`} />
+                    {innerLabel && <div className={`absolute left-4 -z-1 h-full text-neutral ${isFocused || value != "" ? "top-[4px] text-xs " : "top-[14px] text-base"}`}>
+                        {innerLabel}
+                    </div>}
+                </div>
             </div>
         </>
     )
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,10 +4,12 @@ import { Input } from "../../components/ui/inputs/Input"
 
 export const Login = () => {
     const [value , setValue] = useState("")
+    console.log("vlaue in parent" , value);
+    
     return (
         <>
             <div className="flex flex-col gap-5 max-w-3xl mx-auto my-auto h-screen">
-                <Input value={value} onChange={(e)=>setValue(e.value)} label={"password"} placeholder="Password"/>
+                <Input value={value} onChange={(e)=>setValue(e.target.value)}  innerLabel={"Password"} placeholder=""/>
                 <Input value={""} onChange={()=>{}} label={"password"}/>
                 <Button onClick={()=>{}} className=""> Login</Button>
             </div>
